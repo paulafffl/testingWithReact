89fa5e710f7efcf091dc1bcb15afcf61aeb9becb
Paula Lima <paula.lima@mvflp01144.mynet>
2020-06-22 20:01:39 +0100

    Headline component with tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './component/header/index';
+import Headline from './component/headline/index';
 import './../src/app.scss'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <section className='main'>
+        <Headline header='Posts' desc='Click the button'/>
+      </section>  
     </div>
   );
 }
